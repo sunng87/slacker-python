0d1a34bb8df925cc963a6cf2af5b01caa3bbc72a
Sunng <classicning@gmail.com>
2012-04-25 15:09:58 +0800

    first version that works

--- slacker/client.py
+++ slacker/client.py
@@ -1,3 +1,6 @@
+import random
+import itertools
+
 import gevent
 import gevent.socket
 import gevent.event
@@ -10,11 +13,11 @@ class Connection(object):
         self.host = addr.split(":")[0]
         self.port = int(addr.split(":")[1])
         self.sock = None
-        self.transid = 0
+        self.transid = itertools.count()
         self.reqs = {}
 
     def connect(self):
-        self.sock = gevent.socket.create_connection(self.host, self.port)
+        self.sock = gevent.socket.create_connection((self.host, self.port))
         self.clientLoop = gevent.spawn(self.readLoop)
 
     def readLoop(self):
@@ -23,7 +26,7 @@ class Connection(object):
             resp = None
             if packetType == PROTOCOL_PACKET_TYPE_RESPONSE:
                 resp = readResponse(self.sock)
-                resp.desrialize()
+
             elif packetType == PROTOCOL_PACKET_TYPE_ERROR:
                 resp = readError(self.sock)
             cb = self.reqs[tid]
@@ -31,19 +34,52 @@ class Connection(object):
             del self.reqs[tid]
 
     def send(self, request):
-        self.request.serialize()
-        transid = self.transid
+        transid = self.transid.next()
         cb = gevent.event.AsyncResult()
         self.reqs[transid] = cb
-
-        self.transid += 1
 
         buf = StringIO.StringIO()
         writeHeader(buf, transid, PROTOCOL_PACKET_TYPE_REQUEST)
         writeRequest(buf, request)
-        self.sock.send(buf.getvalue())
+        data = buf.getvalue()
+        buf.close()
+
+        self.sock.send(data)
 
         return cb
 
+    def close(self):
+        self.clientLoop.kill()
+        self.sock.close()
+        self.reqs.clear()
 
-    
+class Client(object):
+    def __init__(self, addrs):
+        self.connections = map(lambda m: Connection(m), addrs)
+        for c in self.connections:
+            c.connect()
+
+    def call(self, fname, args):
+        req = SlackerRequest(PROTOCOL_CONTENT_TYPE_CLJ, fname, args)
+        req.serialize()
+        conn = random.choice(self.connections)
+        cb = conn.send(req)
+        result = cb.get()
+        if isinstance(result, SlackerResponse):
+            result.desrialize()
+            return result.body
+        else:
+            code = result.code
+            raise RuntimeError("Error code: " + str(code))
+
+class Proxy(object):
+    def __init__(self, client, namespace):
+        self.client = client
+        self.namespace = namespace
+
+    def __getattr__(self, name):
+        return (lambda *x: self._invoke(name, x))
+
+    def _invoke(self, name, args):
+        return self.client.call(self.namespace+"/"+name, args)
+
--- slacker/protocol.py
+++ slacker/protocol.py
@@ -4,101 +4,101 @@ import clj
 
 PROTOCOL_VERSION = 5
 
-PROTOCOL_HEADER = "bib"
-PROTOCOL_HEADER_SIZE = 6
-
 PROTOCOL_PACKET_TYPE_REQUEST = 0
 PROTOCOL_PACKET_TYPE_RESPONSE = 1
-PROTOCOL_PACKET_TYPE_ERROR = 2
+PROTOCOL_PACKET_TYPE_ERROR = 4
+
+PROTOCOL_CONTENT_TYPE_JSON = 1
+PROTOCOL_CONTENT_TYPE_CLJ = 2
 
 class SlackerRequest(object):
-    type = 0
     def __init__(self, ct, fname, args):
         self.ct = ct
         self.fname = fname
         self.args = args
 
     def serialize(self):
-        serializer = json if self.ct == 1 else clj
+        serializer = json if self.ct == PROTOCOL_CONTENT_TYPE_JSON else clj
         self.args = serializer.dumps(self.args)
 
     def desrialize(self):
-        serializer = json if self.ct == 1 else clj
+        serializer = json if self.ct == PROTOCOL_CONTENT_TYPE_JSON else clj
         self.args = serializer.loads(self.args)
 
 class SlackerResponse(object):
-    type = 1
     def __init__(self, ct, code, body):
         self.ct = ct
         self.code = code
         self.body = body
 
     def serialize(self):
-        serializer = json if self.ct == 1 else clj
+        serializer = json if self.ct == PROTOCOL_CONTENT_TYPE_JSON else clj
         self.body = serializer.dumps(self.body)
 
     def desrialize(self):
-        serializer = json if self.ct == 1 else clj
+        serializer = json if self.ct == PROTOCOL_CONTENT_TYPE_JSON else clj
         self.body = serializer.loads(self.body)
 
 class SlackerError(object):
-    type = 4
     def __init__(self, ct, code):
         self.ct = ct
         self.code = code
 
 def readHeader(fd):
     """read protocol header"""
-    data = fd.read(PROTOCOL_HEADER_SIZE)
-    value = struct.unpack(PROTOCOL_HEADER, data)
+    data = fd.recv(6)
+    value = struct.unpack(">bib", data)
     return value
 
 
 def readRequest(fd):
     """read request body"""
     ## read content type
-    ct = struct.unpack("b", fd.read(1))
+    ct = struct.unpack("b", fd.recv(1))[0]
     
     ## read function name
-    l = struct.unpack("H", fd.read(2))
-    fname = struct.unpack("s", fd.read(l))
+    l = struct.unpack(">H", fd.recv(2))[0]
+    fname = struct.unpack(str(l)+"s", fd.recv(l))[0]
     
     ## read args body
-    l = struct.unpack("I", fd.read(4))
-    args = struct.unpack("s", fd.read(l))
+    l = struct.unpack(">I", fd.recv(4))[0]
+    args = struct.unpack(str(l)+"s", fd.recv(l))[0]
 
     return SlackerRequest(ct, fname, args)
 
 def readResponse(fd):
     """read response body"""
     ## read content type
-    ct = struct.unpack("b", fd.read(1))
+    ct = struct.unpack("b", fd.recv(1))[0]
     
     ## read result code
-    rc = struct.unpack("b", fd.read(1))
+    rc = struct.unpack("b", fd.recv(1))[0]
 
     ## read body
-    l = struct.unpack("I", fd.read(4))
-    body = struct.unpack("s", fd.read(l))
+    l = struct.unpack(">I", fd.recv(4))[0]
+    body = struct.unpack(str(l)+"s", fd.recv(l))[0]
 
     return SlackerResponse(ct, rc, body)
 
 def readError(fd):
     """read error packet body"""
-    rc = struct.unpack("b", fd.read(1))
+    rc = struct.unpack("b", fd.recv(1))
 
     return SlackerError(rc)
 
 def writeHeader(fd, tid, packet_type):
-    fd.write(struct.pack("bib", PROTOCOL_VERSION, tid, packet_type))
+    fd.write(struct.pack(">bib", PROTOCOL_VERSION, tid, packet_type))
 
 def writeResponse(fd, resp):
-    data = struct.pack("bbIs", resp.ct, resp.rc, len(resp.body), resp.body)
-    fd.write(data)
+    bodylen = len(resp.body)
+    fd.write(struct.pack(">bbI", resp.ct, resp.rc, bodylen))
+    fd.write(struct.pack(str(bodylen)+"s", resp.body))
 
 def writeRequest(fd, req):
-    data = struct.pack("bHsIs", req.ct, len(req.fname),
-                       req.fname, len(req.args), req.args)
-    fd.write(data)
+    fnamelen = len(req.fname)
+    argslen = len(req.args)
+    fd.write(struct.pack("b", req.ct))
+    fd.write(struct.pack(">H"+str(fnamelen)+"s", fnamelen, req.fname))
+    fd.write(struct.pack(">I"+str(argslen)+"s", argslen, req.args))
 
 
