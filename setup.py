151e92ecebe64e0e6e694a6ea105d721f87fd8b5
Sun Ning <nsun@avos.com>
2014-03-10 18:23:38 +0800

    fixed README missing

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 long_desc=open('README.rst','r').read()
 
 setup(name="slacker-python",
-      version="0.1.3",
+      version="0.1.4",
       author="Sun Ning",
       author_email="sunng@about.me",
       description="python client of slacker RPC",
@@ -15,6 +15,7 @@ setup(name="slacker-python",
       url="http://github.com/sunng87/slacker-python",
       license='mit',
       packages=['slacker'],
+      package_data={'': ['README.rst']},
       install_requires=['gevent==1.0', 'pyclj==0.2.1'],
       classifiers=['Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
