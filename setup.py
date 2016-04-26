f960c0a68b619fb7d1ccf39b7e933d449ea46240
Ning Sun <sunng@about.me>
2016-04-26 17:08:00 +0800

    (chore) bump version
    Signed-off-by: Ning Sun <sunng@about.me>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 long_desc=open('README.rst','r').read()
 
 setup(name="slacker-python",
-      version="0.4.0-alpha2",
+      version="0.4.0",
       author="Sun Ning",
       author_email="sunng@about.me",
       description="python client of slacker RPC",
